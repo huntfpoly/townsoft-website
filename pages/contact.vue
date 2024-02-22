36b61bda4c3e7065deec9b5543766abb2f2681fa
Hieu Pham <61631054+huntfpoly@users.noreply.github.com>
2024-02-22 13:23:05 +0700

    setup layout (#1)

---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    contact
+  </div>
+</template>
+
+<style>
+
+</style>
--- uno.config.ts
+++ uno.config.ts
@@ -11,8 +11,10 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['btn', 'px-4 py-2 rounded inline-block bg-primary cursor-pointer hover:bg-primary-700 text-white disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50 transition-all	'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['link', 'text-primary-500 hover:text-primary-700 transition-all'],
+    ['menu-item', 'flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-primary-500 transition-all'],
   ],
   presets: [
     presetUno(),
@@ -33,4 +35,20 @@ export default defineConfig({
     transformerDirectives(),
     transformerVariantGroup(),
   ],
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#1a6fff',
+        100: '#e6f0ff',
+        200: '#b3ceff',
+        300: '#80abff',
+        400: '#4d89ff',
+        500: '#1a6fff',
+        600: '#0052cc',
+        700: '#004199',
+        800: '#002b66',
+        900: '#001433',
+      },
+    },
+  },
 })
